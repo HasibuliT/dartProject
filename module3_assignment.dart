3a599c2b3b51354ad949511f5e4207bf4e928f1b
Hasibul Islam <66546794+HasibuliT@users.noreply.github.com>
2023-05-15 21:29:17 +0600

    Update module3_assignment.dart
    Module 3 Assignment...

--- module3_assignment.dart
+++ module3_assignment.dart
@@ -1,1 +1,42 @@
+void main(){
+  Car obj = Car();
+  obj.brand = "BMW Brand";
+  obj.model = "BMW X1";
+  obj.year = 2022;
+  print(obj.brand);
+  print(obj.model);
+  print(obj.year);
+}
+
+class Car{
+  late String brand;
+  late String model;
+  late int year;
+  late double milesDriven;
+
+
+}
+
+void  drive(double miles){
+  millesDrive +=miles;
+
+}
+
+
+double getMilesDriven(){
+  return milesDriven;
+}
+
+String getBrand(){
+  return brand;
+}
+
+int getYear(){
+  return year;
+}
+
+int getAge(){
+  return year;
+}
+
 
